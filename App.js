df330898957f122c796784003fe7d06ed1f3686b
Juan Pablo Romero <jryn145@gmail.com>
2022-11-17 19:59:31 -0500

    base slides component

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Text} from 'react-native';
+import Slides from './components/Slides';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Slides/>
+      <StatusBar style='auto'/>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  }
 });
---
+++ slides.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id:'1',
+        title: 'First Slide',
+        description:'Somos una organizacion sin animo de lucro que busca ayudar a las mascotas a encontrar un hogar',
+        image: require('./assets/animals.png'),
+    
+    },
+    {
+        id:'2',
+        title:'Second Slide',
+        description: 'Podras adoptar a tu mascota sonada o encontrarle un mejor hogar a un gatito rescatado',
+        image: require('./assets/logoFAHPink.png'),
+    },
+    {
+        id:'3',
+        title:'Third Slide',
+        description:'No discriminamos por raza y priorizamos a los que mas tiempo llevan sin un hogar',
+        image: require('./assets/logoFAHYellow.png'),
+    },
+    {
+        id:'4',
+        title:'Fourth Slide',
+        description:'Registrate',
+        image:require('./assets/googleIcon.png'),
+    }
+]
